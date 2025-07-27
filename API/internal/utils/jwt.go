0b06b5e389c888181fa6735218535af4e257bf43
tiago-prog <tiagocorreaneves3@gmail.com>
2025-07-27 18:36:59 -0300

    feat: add jwt function for generation token.

---
+++ API/internal/utils/jwt.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/tiago-prog/novels-api/internal/model"
+)
+
+func GenerateJWT(userID string, role model.Role, secret []byte) (string, error) {
+	if len(secret) < 32 {
+		return "", errors.New("secret must be at least 32 bytes")
+	}
+
+	claims := jwt.MapClaims{
+		"sub":  userID,
+		"role": role,
+		"exp":  time.Now().Add(24 * time.Hour).Unix(),
+		"iat":  time.Now().Unix(),
+		"nbf":  time.Now().Unix(),
+		"iss":  "plataforma-de-novels",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secret)
+}
