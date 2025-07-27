724416008dea10ba7624689875106dc6879fc736
tiago-prog <tiagocorreaneves3@gmail.com>
2025-07-27 18:36:04 -0300

    feat: add new utilites functions for validations.

---
+++ API/internal/utils/helpers.go
@@ -0,0 +1,18 @@
+package utils
+
+import "regexp"
+
+func IsValidEmail(email string) bool {
+	const emailRegex = `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	return re.MatchString(email)
+}
+
+func IsValidUsername(username string) bool {
+	for _, r := range username {
+		if !(r == '_' || (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9')) {
+			return false
+		}
+	}
+	return true
+}
