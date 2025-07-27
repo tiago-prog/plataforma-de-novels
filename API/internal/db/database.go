41b48d6e78b9cff76f106cbcfc147ffba7d7b009
tiago-prog <tiagocorreaneves3@gmail.com>
2025-07-27 18:29:58 -0300

    feat: aadd database connection.

---
+++ API/internal/db/database.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func Connect(host, port, user, password, dbname string) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, db.Ping()
+}
